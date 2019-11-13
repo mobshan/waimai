c2431ff2229bea1569a629eab67261c2ef4528df
mob <yadiraies@gmail.com>
2019-11-13 14:27:17 +0800

    footerNav细节+样式

--- src/components/FooterNav/FooterNav.vue
+++ src/components/FooterNav/FooterNav.vue
@@ -1,13 +1,65 @@
 <template>
-  <footer>
-    <router-link to="/msite">首页</router-link>
-    <router-link to="/search">搜索</router-link>
-    <router-link to="/order">订单</router-link>
-    <router-link to="/profile">我的</router-link>
+  <footer class="footer_guide border-1px">
+    <router-link to="/msite" class="guide_item" v-bind:class="{on: '/msite'===$route.path}">
+      <span class="item_icon">
+        <i class="iconfont icon-icon-test"></i>
+      </span>
+      <span>外卖</span>
+    </router-link>
+
+    <router-link to="/search" class="guide_item">
+      <span class="item_icon">
+        <i class="iconfont icon-search"></i>
+      </span>
+      <span>搜索</span>
+    </router-link>
+    <router-link to="/order" class="guide_item">
+      <span class="item_icon">
+        <i class="iconfont icon-emaxcitygerenxinxitubiaoji03"></i>
+      </span>
+      <span>订单</span>
+    </router-link>
+    <router-link to="/profile" class="guide_item">
+      <span class="item_icon">
+        <i class="iconfont icon-profilehover"></i>
+      </span>
+      <span>我的</span>
+    </router-link>
   </footer>
 </template>
 <script>
 export default {}
 </script>
-<style lang="scss"></style>
-
+<style lang="scss">
+.footer_guide {
+  position: fixed;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  .guide_item {
+    display: flex;
+    flex: 1;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 5px;
+    color: #999999;
+    &.on {
+      color: #02a774;
+    }
+    span {
+      font-size: 12px;
+      margin-top: 2px;
+      margin-bottom: 2px;
+      .iconfont {
+        font-size: 22px;
+      }
+    }
+  }
+}
+</style>
