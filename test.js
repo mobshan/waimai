1641b15bd98b5df315d90f38b206b8cefa895946
mob <yadiraies@gmail.com>
2019-11-20 16:47:58 +0800

    成功获取轮播图数据并循环到页面

---
+++ test.js
@@ -0,0 +1,14 @@
+// 二维数组
+let arr1 = [
+  { title: 'hello' },
+  { title: 'world' },
+  { title: 'hehe' },
+  { title: 'haha' }
+]
+
+let arr2 = [
+  [{ title: 'hello' }, { title: 'world' }],
+  [{ title: 'hehe' }, { title: 'haha' }]
+]
+
+let minArr = []
